770648b7568ea65af88131b123767e889fe115d4
alireza.ab <alirezaabbaszade05@gmail.com>
2022-06-14 18:56:29 +0430

    design home page and create header of site

---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './styles.module.scss'
+const HeaderComponent = () => {
+    return (
+        <div className={styles.container}>
+            <span className="material-icons-battery_std"></span>
+        </div>
+    );
+};
+
+export default HeaderComponent;
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import HeaderComponent from "../header";
 const HomeComponent = () => {
     return (
-        <div>
-            salam
+        <div className={styles.container}>
+            <HeaderComponent/>
+            <picture className={styles.img_background}>
+                <source media="(min-width:1024px)" srcSet="img_pink_flowers.jpg"/>
+                <img src={require("../../assets/images/img.png")} alt="Flowers" />
+            </picture>
         </div>
     );
 };
